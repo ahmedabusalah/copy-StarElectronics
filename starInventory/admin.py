48804c221ca69640dc70cfc9a6b644ba191a6d01
“Sabrian <sabrian.salim@gmail.com>
2022-04-06 10:51:35 +1200

    update model CustomerOrder, ProductPart

--- starInventory/admin.py
+++ starInventory/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from starInventory.models import Customer, Product, Part, Order, ProductPart
+from starInventory.models import Customer, Product, Part, ProductPart, Order, CustomerOrder
 
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Part)
 admin.site.register(Order)
 admin.site.register(ProductPart)
+admin.site.register(CustomerOrder)
--- starInventory/models.py
+++ starInventory/models.py
@@ -12,24 +12,40 @@ class Customer(models.Model):
     )
 
 
-class Product(models.Model):
-    name = models.CharField(null=False, blank=False, max_length=20)
-    price = models.FloatField(null=False, blank=False)
-
-
 class Part(models.Model):
     name = models.CharField(null=False, blank=False, max_length=20)
     stock = models.IntegerField(null=False, blank=False, default=0)
     cost = models.FloatField(null=False, blank=False)
 
 
+class Product(models.Model):
+    name = models.CharField(null=False, blank=False, max_length=20)
+    price = models.FloatField(null=False, blank=False)
+    part = models.ManyToManyField(Part, through='ProductPart')
+
+
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    product = models.ManyToManyField(Product, blank=False)
-    orderQuantity = models.IntegerField(null=False, blank=False, default=1)
+    product = models.ManyToManyField(Product, through='CustomerOrder')
+
+
+class CustomerOrder(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('product', 'order'), name='once_per_customer_order')
+        ]
 
 
 class ProductPart(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    part = models.ManyToManyField(Part, blank=False)
-    quantity = models.IntegerField(null=False, blank=False)
+    part = models.ForeignKey(Part, on_delete=models.CASCADE)
+    quantity = models.IntegerField(null=False, blank=False, default=1)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=('product', 'part'), name='once_per_product_part')
+        ]
